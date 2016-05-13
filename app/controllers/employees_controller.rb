3e8bda2fcce16c6e2ff53679abe2210e41adf14c
Mark Richardson <markeban@yahoo.com>
2016-05-12 19:32:25 -0700

    added .env to gitignore

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,4 +3,8 @@ class EmployeesController < ApplicationController
   def show
     @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
   end
+
+  def index
+    @employees = Unirest.get("http://localhost:3000/api/v1/employees.json").body
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'employees', to: 'employees#index'
   get 'employees/:id', to: 'employees#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
